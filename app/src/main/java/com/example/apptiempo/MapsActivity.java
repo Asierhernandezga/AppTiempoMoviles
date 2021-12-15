11cdffd9dc6495bb2916d4953a9ebc0bcf4609fe
asierhernandezga <asier.hernandezga@elorrieta-errekamari.com>
2021-12-15 09:56:01 +0100

    Pruebas con la ubicacion actual

--- app/src/main/java/com/example/apptiempo/MapsActivity.java
+++ app/src/main/java/com/example/apptiempo/MapsActivity.java
@@ -1,7 +1,13 @@
 package com.example.apptiempo;
 
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -46,10 +52,32 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(ElorrietaLatitud, ElorrietaLongitud);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+        mMap.getUiSettings().setMyLocationButtonEnabled(false);
+
+        LatLng elorrieta = new LatLng(ElorrietaLatitud, ElorrietaLongitud);
+
+        LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        double longitude = location.getLongitude();
+        double latitude = location.getLatitude();
+        LatLng miPosicion = new LatLng(longitude, latitude);
+        mMap.addMarker(new MarkerOptions().position(miPosicion).title("Marcado en MiPosicion"));
+
+        mMap.addMarker(new MarkerOptions().position(elorrieta).title("Marcado en Elorrieta"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(elorrieta));
         mMap.setMinZoomPreference(18);
     }
 }
