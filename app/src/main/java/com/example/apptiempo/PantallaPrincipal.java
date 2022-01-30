df2948ee30f8f357a3a123c635294bbf05d8c6c9
asierhernandezga <asier.hernandezga@elorrieta-errekamari.com>
2022-01-31 00:46:40 +0100

    Idioma ingles

--- app/src/main/java/com/example/apptiempo/PantallaPrincipal.java
+++ app/src/main/java/com/example/apptiempo/PantallaPrincipal.java
@@ -54,7 +54,7 @@ public class PantallaPrincipal extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-    
+
 
     public void abrirProvincias(View view){
         Intent i = new Intent(this, Provincias.class );
